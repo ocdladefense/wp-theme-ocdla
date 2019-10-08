911e7e888d7d8d213d2954a8d6f56de357c67913
Jose Bernal <jbernal.web.dev@gmail.com>
2019-10-08 14:40:33 +0000

    OCDLA updates

--- functions.php
+++ functions.php
@@ -3,42 +3,12 @@
 // Comment this line to use the parent theme's widget areas.
 // add_action( 'widgets_init', 'override_parent_widget_areas' );
 add_action( 'wp_enqueue_scripts', 'init_transparent_header' );
-// add_action( 'wp_enqueue_scripts', 'add_parent_stylesheets' );
-add_action( 'wp_enqueue_scripts', 'add_child_stylesheets' );
-
 
 
 
 
 /**
-  * @function add_parent_stylesheets
-  *
-  * @description Queue up stylesheets for the parent theme.
-
-function add_parent_stylesheets() {
-
-	$basedir = get_template_directory_uri() .'/style-overrides';
-	$styles = array(
-		'general-overrides' => 'style.css',
-		'header-overrides' => 'header.css',
-		'menu-overrides' => 'menu.css',
-		'post-overrides' => 'post.css',
-		'footer-overrides' => 'footer.css'
-	);
-	
-	wp_enqueue_style( 'parent-styles', get_template_directory_uri() . '/style.css' );
-	
-	foreach($styles as $id => $uri) {
-		wp_enqueue_style($id,$basedir.'/'.$uri);
-	}
-	
-}
-  */
-
-
-
-/**
-  * @function add_parent_stylesheets
+  * @function add_child_stylesheets
   *
   * @description Queue up stylesheets for this theme.
   */
@@ -57,17 +27,29 @@ function add_child_stylesheets() {
 		'child.sidebar-overrides' => 'sidebar.css',
 		'child.widget-overrides' => 'widget.css',
 	);
+	$styles = array();
 	
-	wp_enqueue_style( 'child-styles',
-			$basedir . '/style.css',
-			array( 'parent-styles' ),
-			wp_get_theme()->get('Version')
+	wp_enqueue_style(
+		'child-styles',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'parent-styles','menu-overrides'),
+		wp_get_theme()->get('Version')
 	);
     
 	foreach($styles as $id => $uri) {
-		wp_enqueue_style($id,$basedir.'/'.$uri);
+		$dep = explode('.',$id);//Setup dependency with parent theme stylesheet
+		
+		wp_enqueue_style(
+			$id,
+			$basedir.'/'.$uri,
+			array($dep[1]),
+			wp_get_theme()->get('Version')
+		);
 	}
 }
+
+
+
 
 function init_transparent_header() {
 	wp_enqueue_script( 'global-header', get_stylesheet_directory_uri() . '/assets/js/home.js', array('jquery'), true );
---
+++ template-parts/navigation/navigation-top.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage wireframe
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<!--USE ID & CLASSNAME OF "main-navigation" if you want to have the menu collapsible on smaller screens?-->
+<nav class="menu collapsible menu-button" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'wireframe' ); ?>">
+
+	<?php if(has_nav_menu('top')):
+		wp_nav_menu(
+			array(
+				'theme_location' => 'top',
+				'menu_id'        => 'top-menu',
+				'menu_class'		 => 'horizontal light-background'
+			)
+		);
+	endif; ?>
+
+</nav>
