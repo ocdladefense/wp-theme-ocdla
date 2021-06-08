d1bd49d22f5e3951ff8e3c985ef84f6257ab4c51
Ruslan Kalasnikov <rjandson1@hotmail.com>
2021-06-08 10:10:56 -0700

    Changed files

--- contact-committee.php
+++ contact-committee.php
@@ -1,28 +1,28 @@
 <style>
-.primary {
-    border: #50668F;
-}
+    .primary {
+        border: #50668F;
+    }
 
-.primary {
-    color: #FFFFFF;
-}
+    .primary {
+        color: #FFFFFF;
+    }
 
-.primary,
-.primary:hover,
-.primary:focus {
-    background-color: #50668F;
-}
+    .primary,
+    .primary:hover,
+    .primary:focus {
+        background-color: #50668F;
+    }
 
-.primary {
-    background-color: #0070d2;
-    color: white;
-    transition: all 0.1s;
-    border: 1px solid transparent;
-    text-decoration: none;
-    border-radius: 4px;
-    padding: 9px;
-    margin: 10px;
-}
+    .primary {
+        background-color: #0070d2;
+        color: white;
+        transition: all 0.1s;
+        border: 1px solid transparent;
+        text-decoration: none;
+        border-radius: 4px;
+        padding: 9px;
+        margin: 10px;
+    }
 </style>
 
 
@@ -43,6 +43,8 @@
  * @version 1.0
  */
 
+$committees = get_committees_array();
+
 get_header(); ?>
 
 <div class="page-wrap wrap">
@@ -51,66 +53,50 @@ get_header(); ?>
         <main id="main" class="site-main" role="main">
             <br />
 
-            //////////////////////////////////////////////////
-
             <style type="text/css">
-            .table-headers {
-                display: none;
-            }
-
-            #intro {
-                font-size: 18px;
-            }
-
-            h1 {
-                padding-left: 12px;
-                text-transform: uppercase;
-                letter-spacing: 0.14em;
-                font-size: 1.0em;
-            }
-
-            h3 {
-                color: #bf9500;
-            }
-
-            #email {
-                color: rgba(0, 0, 139, 0.8);
-                font-weight: bold;
-            }
-
-            #name {
-                color: grey;
-            }
-
-            #city {
-                color: #53638c;
-                font-weight: 600;
-            }
-
-            p {
-                word-wrap: break-word;
-            }
-
-            @media screen and (min-width: 800px) {
                 .table-headers {
-                    display: table-row;
+                    display: none;
                 }
-            }
-            </style>
 
-            <?php
-            // Helper function that determines wether a member entry should be bold based on a Role
-            function checkRole($member)
-            {
-                $roles = [
-                    "Chair", "Co-chair", "Board Liaison", "President",
-                    "Vice President", "Executive Director", "Legislative Director"
-                ];
-                if (in_array($member["Role"], $roles) || $member["Name"] == "Bob Thuemmel") {
-                    echo 'bold';
+                #intro {
+                    font-size: 18px;
                 }
-            }
-            ?>
+
+                h1 {
+                    padding-left: 12px;
+                    text-transform: uppercase;
+                    letter-spacing: 0.14em;
+                    font-size: 1.0em;
+                }
+
+                h3 {
+                    color: #bf9500;
+                }
+
+                #email {
+                    color: rgba(0, 0, 139, 0.8);
+                    font-weight: bold;
+                }
+
+                #name {
+                    color: grey;
+                }
+
+                #city {
+                    color: #53638c;
+                    font-weight: 600;
+                }
+
+                p {
+                    word-wrap: break-word;
+                }
+
+                @media screen and (min-width: 800px) {
+                    .table-headers {
+                        display: table-row;
+                    }
+                }
+            </style>
 
             <?php $separator = "<span style='color: #bf9500; font-weight: bold;'> | </span>"; ?>
 
@@ -128,48 +114,45 @@ get_header(); ?>
             </div>
 
             <?php if (!isset($committees) || (isset($committees) && count($committees) < 1)) : ?>
-            <ul class="table-row">
-                <li>There are no committees to display.</li>
-            </ul>
+                <ul class="table-row">
+                    <li>There are no committees to display.</li>
+                </ul>
 
             <?php else : ?>
 
-            <?php foreach ($committees as $committee) : ?>
+                <?php foreach ($committees as $committee) : ?>
 
-            <a href="http://sf/page?data=<?php echo $committee["Name"]; ?>">
-                <h3 class="ml-5" style="padding-left: 12px;"><?php print $committee["Name"]; ?></h3>
-            </a>
-            <div class="w-auto ml-5 mt-3 mb-5 mr-5">
-
-                <?php $members = $committee["members"]; ?>
-                <?php foreach ($members as $member) : ?>
-
-                <span style="padding-left: 12px;">
-                    <a href="https://members.ocdla.org/directory/member/<?php print $member["Id"]; ?>" target="_blank"
-                        id="name">
-                        <?php print $member["Name"] . $separator; ?>
+                    <a href="http://sf/page?data=<?php echo $committee["Name"]; ?>">
+                        <h3 class="ml-5" style="padding-left: 12px;"><?php print $committee["Name"]; ?></h3>
                     </a>
-                </span>
-                <span style="font-weight:<?php checkRole($member) ?>;">
-                    <?php substr($member["Role"], -4) != "mber" ? print $member["Role"] . $separator : ""; ?>
-                </span>
-                <span id="city">
-                    <?php $member["City"] != null ? print $member["City"] . $separator : ""; ?>
-                </span>
-                <span>
-                    <?php $email = $member["Email"]; ?>
-                    <?php echo "<a href='mailto:$email' id='email'>Email</a>"; ?>
-                </span>
-                <br>
+                    <div class="w-auto ml-5 mt-3 mb-5 mr-5">
+
+                        <?php $members = $committee["members"]; ?>
+                        <?php foreach ($members as $member) : ?>
+
+                            <span style="padding-left: 12px;">
+                                <a href="https://members.ocdla.org/directory/member/<?php print $member["Id"]; ?>" target="_blank" id="name">
+                                    <?php print $member["Name"] . $separator; ?>
+                                </a>
+                            </span>
+                            <span style="font-weight:<?php print hasPosition($member) ? "bold" : ""; ?>;">
+                                <?php substr($member["Role"], -4) != "mber" ? print $member["Role"] . $separator : ""; ?>
+                            </span>
+                            <span id="city">
+                                <?php $member["City"] != null ? print $member["City"] . $separator : ""; ?>
+                            </span>
+                            <span>
+                                <?php $email = $member["Email"]; ?>
+                                <?php echo "<a href='mailto:$email' id='email'>Email</a>"; ?>
+                            </span>
+                            <br>
+
+                        <?php endforeach; ?>
+
+                    </div>
 
                 <?php endforeach; ?>
-
-            </div>
-
-            <?php endforeach; ?>
             <?php endif; ?>
-
-            ////////////////////////////////////////
 
             <?php
             /*while ( have_posts() ) :
